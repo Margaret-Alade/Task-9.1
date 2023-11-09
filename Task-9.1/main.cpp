2025b8b1e3da8f6487239d497981788779d31f49
Margaret Alade <alade.margaret@yandex.ru>
2023-11-09 09:21:12 +0300

    Initial commit

--- Task-9.1/main.cpp
+++ Task-9.1/main.cpp
@@ -6,9 +6,62 @@
 //
 
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Fraction
+{
+private:
+    int numerator_;
+    int denominator_;
+
+public:
+    Fraction(int numerator, int denominator)
+    {
+        numerator_ = numerator;
+        denominator_ = denominator;
+    }
+    
+    bool operator==(Fraction right) {
+        double M1 = abs(numerator_ / denominator_);
+        double M2 = abs(right.numerator_ / right.denominator_);
+        return M1 == M2;
+    }
+    
+    bool operator!=(Fraction right) {
+        return !(*this == right);
+    }
+    
+    bool operator>(Fraction right) {
+        double M1 = abs(numerator_ / denominator_);
+        double M2 = abs(right.numerator_ / right.denominator_);
+        return M1 > M2;
+    }
+    
+    bool operator<(Fraction right) {
+        return(right > *this);
+    }
+    
+    bool operator>=(Fraction right) {
+        return !(*this < right);
+    }
+    
+    bool operator<=(Fraction right) {
+        return !(*this > right);
+    }
+    
+};
+
+
+int main()
+{
+    Fraction f1(4, 3);
+    Fraction f2(6, 11);
+
+    std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+    std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
     return 0;
 }
